83881b59118e7c0d61b3273549ecdef543417804
AbdElrhman AbdElfattah <abdelrhmanabdelfattah9866@gmail.com>
2024-12-19 18:35:41 +0200

    Refactor Use Case Class

--- lib/Features/home/domain/use_cases/fetch_featured_books_use_case.dart
+++ lib/Features/home/domain/use_cases/fetch_featured_books_use_case.dart
@@ -1,17 +1,20 @@
+import 'package:bookly_app_clean/Core/use-cases/no_param_use_case.dart';
+import 'package:bookly_app_clean/Core/use-cases/use_case.dart';
 import 'package:bookly_app_clean/Features/home/domain/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
 import '../../../../Core/errors/failure.dart';
 import '../entities/book_entity.dart';
 
-class FetchFeaturedBooksUseCase{
+class FetchFeaturedBooksUseCase extends UseCase<List<BookEntity>,NoParam >{
   final HomeRepo homeRepo;
+
   FetchFeaturedBooksUseCase(this.homeRepo);
-  Future<Either<Failure,List<BookEntity>>> fetchFeaturedBooks(){
-    /// check permission
-   return homeRepo.fetchFeaturedBooks();
+
+  @override
+  Future<Either<Failure, List<BookEntity>>> call([NoParam? param]) async{
+    return await homeRepo.fetchFeaturedBooks();
   }
 
 }
 
 
-/// use case achieve single responsibility for solid principles.

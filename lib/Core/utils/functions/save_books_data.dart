23a98c989386466d35d975ef5ded0c02a6bd6235
AbdElrhman AbdElfattah <abdelrhmanabdelfattah9866@gmail.com>
2024-12-30 21:35:47 +0200

    Cashed Featured Books

---
+++ lib/Core/utils/functions/save_books_data.dart
@@ -0,0 +1,9 @@
+import 'package:hive/hive.dart';
+
+import '../../../Features/home/domain/entities/book_entity.dart';
+
+/// this method to save data in hive..
+void saveBooksData(List<BookEntity> books, String boxName) {
+  var box= Hive.box(boxName);
+  box.addAll(books);
+}
--- lib/Features/home/data/data_sources/home_remote_data_source.dart
+++ lib/Features/home/data/data_sources/home_remote_data_source.dart
@@ -1,6 +1,8 @@
 import 'package:bookly_app_clean/Core/utils/api_service.dart';
+import 'package:bookly_app_clean/Core/utils/functions/save_books_data.dart';
 import 'package:bookly_app_clean/Features/home/data/models/book_model/book_model.dart';
 import 'package:bookly_app_clean/Features/home/domain/entities/book_entity.dart';
+import 'package:bookly_app_clean/constants.dart';
 
 abstract class HomeRemoteDateSource{
   Future<List<BookEntity>> fetchFeaturedBooks();
@@ -16,8 +18,12 @@ class HomeRemoteDataSourceImpl extends HomeRemoteDateSource{
   Future<List<BookEntity>> fetchFeaturedBooks() async{
     var data = await apiService.get(endpoint: "volumes?Filtering=free-ebooks&q=programming");
     List<BookEntity> books = getBookList(data);
+    /// you stored data in hive to used in local data source after fetch data ..
+    saveBooksData(books, kFeaturedBox);
     return books;
   }
+
+
 
   @override
   Future<List<BookEntity>> fetchNewestBooks() async{

9140a2f11141597fd681db79abdb289f983e2c9b
AbdElrhman AbdElfattah <abdelrhmanabdelfattah9866@gmail.com>
2025-01-02 16:39:25 +0200

    Implementation Fetch Newest Books Cubit

---
+++ lib/Features/home/presentation/manager/fetch_newest_books_cubit/newest_books_cubit.dart
@@ -0,0 +1,17 @@
+import 'package:bookly_app_clean/Features/home/domain/use_cases/fetch_newest_book_use_case.dart';
+import 'package:bookly_app_clean/Features/home/presentation/manager/fetch_newest_books_cubit/newest_books_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class NewestBooksCubit extends Cubit{
+  NewestBooksCubit(this.fetchNewestBooksUseCase): super(NewestBooksInitial());
+  final FetchNewestBooksUseCase fetchNewestBooksUseCase;
+  Future<void> fetchFeaturedBooks()async {
+    emit(NewestBooksLoading());
+    var result=  await fetchNewestBooksUseCase.call();
+    result.fold((failure){
+      emit(NewestBooksFailure(failure.errorMessage));
+    },(books){
+      emit(NewestBooksSuccess(books));
+    });
+  }
+}
---
+++ lib/Features/home/presentation/manager/fetch_newest_books_cubit/newest_books_state.dart
@@ -0,0 +1,14 @@
+import 'package:bookly_app_clean/Features/home/domain/entities/book_entity.dart';
+
+abstract class NewestBooksState{}
+class NewestBooksInitial extends NewestBooksState{}
+class NewestBooksLoading extends NewestBooksState{}
+class NewestBooksFailure extends NewestBooksState{
+  final String errMessage ;
+  NewestBooksFailure(this.errMessage);
+}
+class NewestBooksSuccess extends NewestBooksState{
+  final List<BookEntity>books;
+  NewestBooksSuccess(this.books);
+
+}
